d550a5eb4458571af02e810b3f5a5603a7965c3b
shyhyawJou <Mateo.Jou@primax.com.tw>
2022-12-10 15:59:45 +0800

    upload

---
+++ ig.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+    
+import tensorflow as tf
+
+
+
+class Integrated_Gradient:
+    def __init__(self, model, device, preprocess=None, steps=20):
+        self.model = model
+        self.device = 'GPU' if device == 'cuda' else 'CPU'
+        self.prep = preprocess
+        self.steps = steps
+    
+    def get_heatmap(self, img, baseline=None):
+        with tf.device(self.device):
+            # check whether the input shape is the same as baseline
+            x = img[None] if self.prep is None else self.prep(img)[None]                
+            if baseline is None:
+                baseline = tf.zeros_like(x)
+            self._check(x, baseline)                
+            
+            # get predict label
+            output = self.model(x)
+            pred_label = tf.argmax(output, 1).numpy().item()
+            
+            # compute integrated gradient
+            with tf.GradientTape() as tape:
+                x = tf.Variable(x)
+                X, delta_X = self._get_X_and_delta(x, baseline, self.steps)
+                ig_grad = tape.gradient(self.model(X)[:, pred_label], X)
+                ig_grad = delta_X * (ig_grad[:-1] + ig_grad[1:]) / 2.          
+                ig_grad = tf.reduce_sum(ig_grad, axis=0)
+
+            # plot
+            cam = tf.nn.relu(ig_grad)
+            cam = cam / tf.reduce_max(cam) * 255.
+            cam = cam.numpy().astype(np.uint8)
+            cam = cv2.resize(cam, img.shape[:2][::-1])
+            cam = cv2.applyColorMap(cam, cv2.COLORMAP_JET)[..., ::-1]       
+                            
+        return output, cam
+
+    def _get_X_and_delta(self, x, baseline, steps):
+        alphas = tf.cast(tf.linspace(0, 1, steps + 1), 'float32')
+        alphas = tf.reshape(alphas, (-1, 1, 1, 1))
+        delta_x = (x - baseline)
+        x = baseline + alphas * delta_x
+        return x, delta_x / steps
+    
+    def _check(self, x, baseline):
+        if x.shape != baseline.shape:
+            raise ValueError(f'input shape should equal to baseline shape. '
+                             f'Got input shape: {x.shape}, '
+                             f'baseline shape: {baseline.shape}') 
+
---
+++ show.py
@@ -0,0 +1,62 @@
+from PIL import Image
+import argparse
+import numpy as np
+
+import tensorflow as tf
+
+from ig import Integrated_Gradient
+
+
+
+def get_arg():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', help='tensorflow model path (keras saved model folder)')
+    parser.add_argument('-d', default='cpu', help='device ("cpu" or "cuda")')
+    parser.add_argument('-img', help='img path')
+    parser.add_argument('-step', default=20, type=int, help='number of steps in the path integral')
+    return parser.parse_args()
+    
+
+
+def main():
+    arg = get_arg()
+
+    preprocess = tf.keras.Sequential([
+        tf.keras.layers.Resizing(224, 224),
+        tf.keras.layers.Rescaling(1 / 127.5, -1),
+    ])
+   
+    device = arg.d
+    if device == 'cuda' and len(tf.config.list_physical_devices('GPU')) == 0:
+        raise ValueError('There is no cuda !!!')
+    
+    if arg.m is None:
+        model = tf.keras.applications.MobileNetV2(classifier_activation=None)
+    else:
+        model = tf.keras.models.load_model(arg.m)
+    model.summary()
+        
+    ig_obj = Integrated_Gradient(model, arg.d, preprocess, arg.step)
+    
+    print('\ndevice:', arg.d)
+    print('img:', arg.img)
+    
+    img = Image.open(arg.img).convert('RGB')
+    w, h = img.size
+    img = tf.constant(np.array(img))
+    
+    # output is tf Tensor, heatmap is ndarray
+    output, heatmap = ig_obj.get_heatmap(img, baseline=None)
+    print('\nPredict label:', np.argmax(output, 1).item())
+    
+    img = Image.fromarray(img.numpy())
+    heatmap = Image.fromarray(heatmap)
+    result = Image.new('RGB', (2 * w, h))
+    result.paste(img)
+    result.paste(heatmap, (w, 0))
+    result.show()
+
+
+
+if __name__ == "__main__":
+    main()
